d1eb700243b20408554900a22452e763bc1122c4
Cicera Rodrigues <cicera_mrodrigues@hotmail.com>
2016-12-12 00:45:04 -0200

    Questão2

---
+++ Circuito.sv
@@ -0,0 +1,19 @@
+module Circuito(
+input Clock, output logic f);
+    logic[8:0] count, EntradaCount, EntradaSaida;
+
+    Registrador Count(.Clock(Clock), .Entrada(EntradaCount), .Saida(count)),
+                Saida(.Clock(Clock), .Entrada(EntradaSaida), .Saida(f));
+
+    always_comb begin
+        if(count==9'd499)
+            EntradaCount = 9'd0;
+        else
+            EntradaCount = count + 9'd1;
+
+        if(count>9'd19 && count<9'd90)
+            EntradaSaida = 9'd0;
+        else
+            EntradaSaida = 9'd1;
+    end
+endmodule
--- Registrador.sv
+++ Registrador.sv
@@ -1,8 +1,8 @@
 module Registrador(
-input Clock, 
-input[3:0] Entrada, 
-output logic[3:0] Saida);
- 
-    always_ff @(posedge Clock)
+input Clk, 
+input[8:0] Entrada, 
+output logic[8:0] Saida);
+
+    always_ff @(posedge Clk)
         Saida <= Entrada;
 endmodule
